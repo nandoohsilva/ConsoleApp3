4de506f920a7c25355f8fa7ccf23465e1f0d92ec
Fernando <nandoohsilva@gmail.com>
2023-04-23 11:15:06 -0300

    Inserindo dados de cpf

--- Contas/LibClasses/Carteira.cs
+++ Contas/LibClasses/Carteira.cs
@@ -13,36 +13,49 @@ namespace Contas.LibClasses
             get;
             private set;
         }
-        public string Dono { get; set; }
+        public string Dono 
+        { 
+            get; 
+            set;
+        }// get= busca, set= define.
 
-        public int Numero_da_conta { get; set; }
+        public int Numero_da_conta 
+        { 
+            get;
+            set;
+        }// Adciona um numero a conta
 
-        public string Cpf { get; set; }
-
-        public bool Sacar(double Valor)
+        public string Cpf 
         {
-            if (Valor > this.Saldo)
+            get;
+            set; 
+        }// adciona um cpf a conta
+
+        public bool Sacar(double Valor)//método sacar
+        {
+            if (Valor > this.Saldo)// se valor maior que saldo, retorna false
                 return false;
 
-            this.Saldo -= Valor;
+            this.Saldo -= Valor;// saldo - valor, return verdadeiro
             //this.Saldo = Saldo - Valor;
             return true;
         }
 
         public bool Depositar(double Valor)
         {
-            this.Saldo += Valor;
+            this.Saldo += Valor;// saldo + valor, return true, adciona um valor ao saldo.
             return true;
         }
 
         public bool Transferir
-            (Carteira destino, double valor)
+            (Carteira destino, double valor)// destino carteira, valor tipo double
         {  
             //se nao tiver saldo cancela transferencia retornando false
             if (this.Saldo <= valor)
                 return false;
 
-            //Executa transferencia tirando da conta origram e deposinto na conta destino
+            
+            //Executa transferencia tirando da conta origem e deposita na conta destino
             this.Sacar(valor);
             bool tOK = destino.Depositar(valor);
             if (tOK)// se transferencia ocorreu com sucesso retorna true
